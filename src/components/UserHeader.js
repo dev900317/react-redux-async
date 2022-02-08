6d33cd7420a7d879b8ec700f489aa90cbdaf3dac
dev900317 <bartosz.zagorski@icloud.com>
2022-02-08 17:31:02 +0100

    Refactor userReducer

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,19 +8,18 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => this.props.userId === user.id);
+    const { user } = this.props;
 
     if (!user) {
       return null;
     }
+
     return <div>{user.name}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.users,
-  };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
